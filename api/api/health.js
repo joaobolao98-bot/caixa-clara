a2b264c50e2b29ac9bd2f5bfa36bc41be623c29e
Joao Carlos <joaobolao98@gmail.com>
2026-09-20 11:29:52 -0300

    Delete api/api/health.js

--- api/api/health.js
+++
@@ -1,6 +0,0 @@
-export default function handler(req, res) {
-  return res.status(200).json({
-    has_mp_access_token: Boolean(process.env.MP_ACCESS_TOKEN),
-    node_env: process.env.NODE_ENV
-  });
-}
